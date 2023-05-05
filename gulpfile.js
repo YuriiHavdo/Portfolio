cea1b3fc000140e774c62bea19746db1f1cfb0ff
Yurii Havdo <havdoyurii@hotmail.com>
2023-05-05 15:14:33 +0200

    +task

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,32 @@
-var gulp = require('gulp');
-var rename = require('gulp-rename');
-var sass = require('gulp-sass')(require('sass'));
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const htmlmin = require('gulp-htmlmin');
+
 
 function css_style (done) {
-    gulp.src('./sass/style.scss')
+    gulp.src('./scss/style.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass({
         errorLogToConsole: true,
         // outputStyle: 'compressed'
     }))
     .on('erorr', console.error.bind(console))
-    // .pipe(rename({suffix: '.min'}))           
+    .pipe(autoprefixer({
+        browers: ['last 2 version'],
+        cascade: false
+    }))
+    // .pipe(rename({suffix: '.min'}))  
+    .pipe(sourcemaps.write('./'))    
     .pipe(gulp.dest('./css/'));
     done();
 }
 
+
+function html() {
+    
+}
 
 gulp.task(css_style)
