cc32e5af947c5c58ff9080e7bcf8662c076f45a9
NickZmh <nickzmh92@gmail.com>
2023-06-08 21:08:27 +0200

    added the index js and settings

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const works = [
+    {
+        id: '1',
+        title: 'Designing Dashboards',
+        year: '2020',
+        category: 'Dashboard',
+        description: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+        img: 'https://via.placeholder.com/600/771796'
+    },
+    {
+        id: '2',
+        title: 'Vibrant Portraits of 2020',
+        year: '2019',
+        category: 'Illustration',
+        description: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+        img: 'https://via.placeholder.com/600/24f355'
+    },
+    {
+        id: '3',
+        title: '36 Days of Malayalam type',
+        year: '2018',
+        category: 'Typography',
+        description: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+        img: 'https://via.placeholder.com/600/f66b97'
+    },
+    {
+        id: '4',
+        title: 'Components',
+        year: '2017',
+        category: ['Components', 'Design'],
+        description: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+        img: 'https://via.placeholder.com/600/56a8c2'
+    }
+]
+
+
